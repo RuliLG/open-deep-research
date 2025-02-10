136448484ece13e792581fbc924486e45368839b
Raúl López <hi@raullg.com>
2025-02-10 17:35:59 +0000

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,36 @@
+import { NextResponse } from 'next/server';
 import { type NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {}
+const [AUTH_USER, AUTH_PASS] = (process.env.HTTP_BASIC_AUTH || ':').split(':');
+
+export async function middleware(request: NextRequest) {
+  if (!isAuthenticated(request)) {
+    return new NextResponse('Authentication required', {
+      status: 401,
+      headers: { 'WWW-Authenticate': 'Basic' },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+function isAuthenticated(request: NextRequest) {
+  const authheader = request.headers.get('authorization') || request.headers.get('Authorization');
+
+  if (!authheader) {
+    return false;
+  }
+
+  const auth = Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+
+  if (user == AUTH_USER && pass == AUTH_PASS) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 export const config = {
   matcher: [
